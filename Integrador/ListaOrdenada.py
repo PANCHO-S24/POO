134d77bd4472266a19bcc5e6df58c1a8cb7de072
PANCHO-S24 <132851065+PANCHO-S24@users.noreply.github.com>
2023-06-06 19:11:09 -0300

    Delete Integrador directory

--- Integrador/ListaOrdenada.py
+++
@@ -1,9 +0,0 @@
-import ManejadorAlumnos
-import ManejadorMaterias
-
-def MostrarLisOrdenado(arreglo, lista):
-    ListPorApell = arreglo.obtenerListOrdenadaApelldo()
-    ListPorApell.sort()
-    print("Lista ordenada de alumnos Alfabeticamente: ")
-    for i in range(len(ListPorApell)):
-        print(ListPorApell[i])
